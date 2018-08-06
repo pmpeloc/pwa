35f99260df5f3848a14da34f3a2f0eaffc3536f6
Pablo Misael Peloc <pmpeloc@gmail.com>
2018-08-06 02:01:57 -0300

    Primer commit de la PWA

---
+++ main.js
@@ -0,0 +1,25 @@
+//Service Worker
+
+if('serviceWorker' in navigator){
+  console.log('Puedes usar los serviceWorker en tu navegador');
+
+  navigator.serviceWorker.register('./sw.js')
+                         .then(res => console.log('serviceWorker cargado correctamente', res))
+                         .catch(err => console.log('serviceWorker no se ha podido registrar', err));
+} else {
+  console.log('No puedes usar serviceWorker en tu navegador');
+}
+
+
+// Scroll Suavizado
+$(document).ready(function() {
+  $("#menu a").click(function(e) {
+    e.preventDefault();
+
+    $("html, body").animate({
+      scrollTop: $($(this).attr('href')).offset().top
+    });
+
+    return false;
+  });
+});
---
+++ sw.js
@@ -0,0 +1,86 @@
+// Asignar nombre y version de la cache
+const CACHE_NAME = 'v1_cache_bycode_pwa';
+
+//Ficheros a cachear en la aplicación
+var urlsToCache = [
+  './',
+  './css/styles.css',
+  './img/favicon.png',
+  './img/1.png',
+  './img/2.png',
+  './img/3.png',
+  './img/4.png',
+  './img/5.png',
+  './img/6.png',
+  './img/facebook.png',
+  './img/instagram.png',
+  './img/twitter.png',
+  './img/favicon-1024.png',
+  './img/favicon-512.png',
+  './img/favicon-384.png',
+  './img/favicon-256.png',
+  './img/favicon-192.png',
+  './img/favicon-128.png',
+  './img/favicon-96.png',
+  './img/favicon-64.png',
+  './img/favicon-32.png',
+  './img/favicon-16.png'
+];
+
+// Evento Install
+// instalacion del serviceWorker y guardar en cache los recursos estaticos de la aplicacion
+
+self.addEventListener('install', e => {
+    e.waitUntil(
+      caches.open(CACHE_NAME)
+            .then(cache => {
+              return cache.addAll(urlsToCache)
+                          .then(() => {
+                            self.skipWaiting();
+                          });
+            })
+            .catch(err => console.log('No se ha registrado la cache', err))
+    );
+});
+
+// Envento Activate
+// Que la app funcione sin conexión
+self.addEventListener('activate', e => {
+  const cacheWhitelist = [CACHE_NAME];
+
+  e.waitUntil(
+    caches.keys()
+          .then(cacheNames => {
+            return Promise.all(
+              cacheNames.map(cacheName => {
+                if(cacheWhitelist.indexOf(cacheName) === -1) {
+                  // Borrar elementos que no se necesitan
+                  return caches.delete(cacheName);
+                }
+              })
+            );
+          })
+          .then(() => {
+            //Activar cache
+            self.clients.claim();
+          })
+  );
+});
+
+
+// Evento Fetch
+self.addEventListener('fetch', e => {
+
+  e.respondWith(
+    caches.match(e.request)
+          .then(res => {
+            if(res) {
+              //Devuelvo datos desde cache
+              return res;
+            }
+
+            return fetch(e.request);
+          })
+  );
+
+});
